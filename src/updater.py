8a9ae4693581c0c876d1f9517800356bb07c1aed
electricboy6 <117548367+electricboy6@users.noreply.github.com>
2023-05-25 10:37:45 -0500

    Add timeouts to requests calls

--- src/updater.py
+++ src/updater.py
@@ -3,13 +3,13 @@ from zipfile import ZipFile
 
 def checkForUpdate():
     print("Checking for updates...")
-    v = requests.get("https://raw.githubusercontent.com/electricboy6/WinRepairTool/main/program.version", allow_redirects=True)
+    v = requests.get("https://raw.githubusercontent.com/electricboy6/WinRepairTool/main/program.version", allow_redirects=True, timeout=5)
     currentV = open("source.version").read()
     if not v == currentV:
         print("Found update - Downloading...")
-        r = requests.get("https://api.github.com/repos/electricboy6/WinRepairTool/releases/latest")
+        r = requests.get("https://api.github.com/repos/electricboy6/WinRepairTool/releases/latest", allow_redirects=True, timeout=5)
         version = r.json()["name"]
-        r = requests.get("https://github.com/electricboy6/WinRepairTool/releases/tag/" + version)
+        r = requests.get("https://github.com/electricboy6/WinRepairTool/releases/tag/" + version, allow_redirects=True, timeout=5)
         open("src/newVersion/source.zip", "wb").write(r.content)
         print("Extracting...")
         with ZipFile("src/newVersion/source.zip", "r") as zip:
